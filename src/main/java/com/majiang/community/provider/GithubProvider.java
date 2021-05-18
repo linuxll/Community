7339ba0bbae2867b29f6c51b629b6d15789b554f
linuxll <534113735@qq.com>
2021-05-18 23:16:29 +0800

    second sign in

--- src/main/java/com/majiang/community/provider/GithubProvider.java
+++ src/main/java/com/majiang/community/provider/GithubProvider.java
@@ -8,19 +8,19 @@ import java.io.IOException;
 
 @Component
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        MediaType JSON
-                = MediaType.get("application/json; charset=utf-8");
-
-        OkHttpClient client = new OkHttpClient();
-
-        RequestBody body = RequestBody.create(JSON, json);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
+//    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+//        MediaType JSON
+//                = MediaType.get("application/json; charset=utf-8");
+//
+//        OkHttpClient client = new OkHttpClient();
+//
+//        RequestBody body = RequestBody.create(JSON, json);
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .post(body)
+//                .build();
+//        try (Response response = client.newCall(request).execute()) {
+//            return response.body().string();
+//        }
+//    }
 }
