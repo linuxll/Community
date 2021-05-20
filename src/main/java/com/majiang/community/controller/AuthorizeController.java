fe89b737e58c78204e0a2e48998b2caa73a27648
linuxll <534113735@qq.com>
2021-05-20 20:45:14 +0800

    h2 database

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.majiang.community.controller;
 
 import com.majiang.community.dto.AccessTokenDTO;
 import com.majiang.community.dto.GithubUser;
+import com.majiang.community.mapper.UserMapper;
+import com.majiang.community.model.User;
 import com.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,6 +28,9 @@ public class AuthorizeController {
 
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
@@ -42,6 +48,13 @@ public class AuthorizeController {
         GithubUser githubUser=githubProvider.getUser(str);
         if(githubUser!=null){
             //成功登录
+            User user=new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else{
